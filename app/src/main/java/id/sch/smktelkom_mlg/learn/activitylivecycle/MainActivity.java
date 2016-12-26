efc83072b12b847ff931d361027049f7e8d359e2
Naufal Dwi <naufal_prakoso_24rpl@student.smktelkom-mlg.sch.id>
2016-12-26 21:05:55 +0700

    Livecycle Music

--- app/src/main/java/id/sch/smktelkom_mlg/learn/activitylivecycle/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/activitylivecycle/MainActivity.java
@@ -1,5 +1,6 @@
 package id.sch.smktelkom_mlg.learn.activitylivecycle;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -7,6 +8,7 @@ import android.util.Log;
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "LivecycleTag";
+    MediaPlayer mediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,46 +17,47 @@ public class MainActivity extends AppCompatActivity {
 
         Log.d(TAG, "onCreate: ");
 
-        @Override
-        protected void onStart ()
-        {
-            super.onStart();
-            Log.d(TAG, "onStart: ");
-        }
+        mediaPlayer = MediaPlayer.create(this, R.raw.song);
 
-        @Override
-        protected void onResume ()
-        {
-            super.onResume();
-            Log.d(TAG, "onResume: ");
-        }
+    }
 
-        @Override
-        protected void onPause ()
-        {
-            super.onPause();
-            Log.d(TAG, "onPause: ");
-        }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart: ");
+    }
 
-        @Override
-        protected void onStop ()
-        {
-            super.onStop();
-            Log.d(TAG, "onStop: ");
-        }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume: ");
+        mediaPlayer.start();
+    }
 
-        @Override
-        protected void onRestart ()
-        {
-            super.onRestart();
-            Log.d(TAG, "onRestart: ");
-        }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause: ");
+        mediaPlayer.pause();
+    }
 
-        @Override
-        protected void onDestroy ()
-        {
-            super.onDestroy();
-            Log.d(TAG, "onDestroy: ");
-        }
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop: ");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG, "onRestart: ");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy: ");
+        if (mediaPlayer != null)
+            mediaPlayer.release();
     }
 }
